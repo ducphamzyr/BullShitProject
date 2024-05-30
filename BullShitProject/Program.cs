ee2b092a003d69b39a7ce33fec7ae6f75f800791
ZyrM <tduck.contact@gmail.com>
2024-05-31 02:43:53 +0700

    Add project files.

---
+++ BullShitProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullShitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Hello World");
+        }
+        private static void Print(string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+        // =))))))))))))))))))
+    }
+}
